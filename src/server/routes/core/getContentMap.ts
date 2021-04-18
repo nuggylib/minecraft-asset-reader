035a7a4692d23ad1c0d4c509a60c420c990df606
ax-vasquez <ar.xa.vasquez@gmail.com>
2021-04-17 23:05:41 -0500

    Stuff to support sidebar logic

--- src/server/routes/core/getContentMap.ts
+++ src/server/routes/core/getContentMap.ts
@@ -3,8 +3,6 @@ import { CACHE } from "../../../main"
 
 export function getContentMap(req: express.Request, res: express.Response) {
   return CACHE.getContentMapFromCache().then((contentMap) => {
-    res.send({
-      contentMap,
-    })
+    res.send(contentMap)
   })
 }
--- src/server/routes/core/setContentMapNamespaceBlocks.ts
+++ src/server/routes/core/setContentMapNamespaceBlocks.ts
@@ -22,8 +22,6 @@ export function setContentMapNamespaceBlocks(
     namespace,
     blocks,
   }).then((contentMap) => {
-    res.send({
-      contentMap,
-    })
+    res.send(contentMap)
   })
 }
